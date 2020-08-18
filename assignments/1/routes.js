091023875aea54275d6b56105c9bbe929b780dfb
shihs <mcshihs@gmail.com>
2020-08-18 21:12:01 +0800

    modified assigment 1

--- assignments/1/routes.js
+++ assignments/1/routes.js
@@ -27,7 +27,6 @@ const requestHandler = (req, res) => {
 		res.write('</body>');
 		res.write('</html>');
 		return res.end();
-
 	}
 
 	if (url === '/create-user' && method === 'POST') {
@@ -39,14 +38,14 @@ const requestHandler = (req, res) => {
 			// console.log(chunk);
 			body.push(chunk); // push a new element
 		});
-		return req.on('end', () => {
+		req.on('end', () => {
 			const parseBody = Buffer.concat(body).toString();			
 			const username = parseBody.split('=')[1];
 			console.log(username);
-			res.statusCode = 302; // 302 stands for redirection
-			res.setHeader('Location', '/');
-			return res.end();
 		});
+		res.statusCode = 302; // 302 stands for redirection
+		res.setHeader('Location', '/');
+		return res.end();
 	}
 };
 
